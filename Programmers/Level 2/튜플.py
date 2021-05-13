2f437780962869081a4ad36287caae5012889a63
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-05-13 23:38:11 +0900

    Programmers 튜플 Level2

---
+++ Programmers/Level 2/튜플.py
@@ -0,0 +1,20 @@
+
+def solution(s):
+    answer = []
+    s = s[2:-2]
+    s = s.split("},{")
+    s.sort(key=len)
+
+    for i in s:
+        num = i.split(",")
+        print(num)
+        for j in num:
+            n = int(j)
+            print(n)
+            if n not in answer:
+                answer.append(n)
+
+    return answer
+
+s = "{{20,111},{111}}"
+print(solution(s))
