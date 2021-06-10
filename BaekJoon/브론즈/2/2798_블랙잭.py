67d6fbdf7862c59cec34f4e611aa58f9293c1f6d
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-06-10 21:26:41 +0900

    BJ 2798 블랙잭 B2(추가풀이)

--- BaekJoon/브론즈/2/2798_블랙잭.py
+++ BaekJoon/브론즈/2/2798_블랙잭.py
@@ -12,3 +12,17 @@ for i in range(0, L-2):
                 sum = max(sum, num[i] + num[j] + num[k])
 
 print(sum)
+
+# def solution(n,m,c):
+#     t = set()
+#     for i in range(n-2):
+#         for j in range(i+1, n-1):
+#             for k in range(j+1, n):
+#                 s = c[i] + c[j] + c[k]
+#                 if s <= m:
+#                     t.add(s)
+#                     break
+#
+#     return max([*t])
+#
+# print(solution(*map(int,input().split()),list(sorted(map(int,input().split()))[::-1])))
