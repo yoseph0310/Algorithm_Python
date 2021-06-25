c3059e0035792bc6efe6421e148d13778d3349e7
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-06-25 15:10:50 +0900

    Programmers 시저암호

--- Programmers/Level 1/시저 암호.py
+++ Programmers/Level 1/시저 암호.py
@@ -12,3 +12,12 @@ def solution(s, n):
         else:
             answer += ' '
     return answer
+
+def solution2(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr((ord(s[i]) - ord('A') + n) % 26 + ord('A'))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
+    return "".join(s)
