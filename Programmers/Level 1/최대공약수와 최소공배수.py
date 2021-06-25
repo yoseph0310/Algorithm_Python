d598b2efb8469f61458fc720291b612b0453177d
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-06-25 14:16:28 +0900

    Programmers 최대공약수와 최소공배수

---
+++ Programmers/Level 1/최대공약수와 최소공배수.py
@@ -0,0 +1,9 @@
+def solution(n,m):
+    def gcd(n,m):
+        while m:
+            n,m = m, n%m
+        return n
+    def lcm(n,m):
+        return (n*m)//gcd(n,m)
+    answer = [gcd(n,m), lcm(n,m)]
+    return answer
