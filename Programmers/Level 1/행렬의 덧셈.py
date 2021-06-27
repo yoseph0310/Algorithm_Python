4ac88a4dc61ea725285247b28290ca5d31635e1d
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-06-27 16:38:58 +0900

    Programmers 행렬의 덧셈

--- Programmers/Level 1/행렬의 덧셈.py
+++ Programmers/Level 1/행렬의 덧셈.py
@@ -1,9 +1,9 @@
 def solution(A,B):
-    answer = []
+    a = []
 
     for i in range(len(A)):
         tmp = []
         for j in range(len(A[i])):
             tmp.append(A[i][j] + B[i][j])
-        answer.append(tmp)
-    return answer
+        a.append(tmp)
+    return a
