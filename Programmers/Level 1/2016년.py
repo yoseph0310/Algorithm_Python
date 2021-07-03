3e24ab8f0c6e6173cd8af1301909bfaecca9d5cf
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-07-03 19:32:41 +0900

    Programmers 2016년

--- Programmers/Level 1/2016년.py
+++ Programmers/Level 1/2016년.py
@@ -1,6 +1,6 @@
 import datetime
 
-def solution(a,b):
+def solution(a, b):
     days = ['MON','TUE','WED','THU','FRI','SAT','SUN']
     answer = days[datetime.date(2016,a,b).weekday()]
     return answer
