b74da35274fe7ff12260b837ae14ff2f9ad11773
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-07-05 23:53:13 +0900

    Programmers 소수찾기

---
+++ Programmers/Level 1/소수찾기.py
@@ -0,0 +1,17 @@
+def sol1(n):
+    sieve = [True] * (n+1)
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i]:
+            for j in range(i+i, n+1, i):
+                sieve[j] = False
+
+    return len([i for i in range(2, n+1) if sieve[i]])
+
+def sol2(n):
+    num = set(range(2, n+1))
+    for i in range(2, n+1):
+        if i in num:
+            num -= set(range(2*i, n+1, i))
+
+    return len(num)
