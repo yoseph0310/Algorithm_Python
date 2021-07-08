e418c5219a63c60b8c2e8609a72f394026c748f7
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-07-08 16:19:11 +0900

    Programmers KAKAO BLIND RECRUITMENT [1차] 비밀지도 - 다른 풀이

--- Programmers/Level 1/[1차] 비밀지도.py
+++ Programmers/Level 1/[1차] 비밀지도.py
@@ -9,3 +9,14 @@ def solution(n, arr1, arr2):
         answer.append(tmp)
 
     return answer
+
+def solution2(n, arr1, arr2):
+    answer = []
+    for num1, num2 in zip(arr1, arr2):
+        tmp = bin( num1 | num2 )[2:]
+        tmp = tmp.rjust(n,'0')
+        tmp = tmp.replace('1','#')
+        tmp = tmp.replace('0',' ')
+        answer.append(tmp)
+
+    return answer
