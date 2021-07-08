e879c8061ab2141279594d965079c940871fbbd5
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-07-08 16:56:25 +0900

    Programmers 실패율

--- Programmers/Level 1/실패율.py
+++ Programmers/Level 1/실패율.py
@@ -1,6 +1,21 @@
 def solution(N, stages):
     answer = []
+    len_stages = len(stages)
+    for i in range(1, N+1):
+        cnt = stages.count(i)
+        if cnt == 0:
+            fail = 0
+        else:
+            fail = cnt / len_stages
+        answer.append((i, fail))
+        len_stages -= cnt
+
+    answer = sorted(answer, key=lambda t:t[1], reverse=True)
+    answer = [i[0] for i in answer]
+
     return answer
+
+print(solution(5, [2,1,2,6,2,4,3,3]))
 
 
 
