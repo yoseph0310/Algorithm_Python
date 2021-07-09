246831440fd69b4838063596fa7a9c6281a5ac4b
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-07-09 20:03:56 +0900

    Programmers 숫자 문자열과 영단어

---
+++ Programmers/Level 1/숫자 문자열과 영단어.py
@@ -0,0 +1,22 @@
+def sol(s):
+    s = s.replace('zero', '0')
+    s = s.replace('one', '1')
+    s = s.replace('two', '2')
+    s = s.replace('three', '3')
+    s = s.replace('four', '4')
+    s = s.replace('five', '5')
+    s = s.replace('six', '6')
+    s = s.replace('seven', '7')
+    s = s.replace('eight', '8')
+    s = s.replace('nine', '9')
+
+    return int(s)
+
+def solution(s):
+    eng = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    for i, n in enumerate(eng):
+        if n in s:
+            s = s.replace(n, str(i))
+    return int(s)
+
+print(solution('onetwothreefourfive6seven8nine'))
