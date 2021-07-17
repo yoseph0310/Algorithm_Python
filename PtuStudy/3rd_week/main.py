88ed4b7c11e5523ffa78aad22879caa660630e7c
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-07-17 23:27:10 +0900

    PtuStudy dir 추가

---
+++ PtuStudy/3rd_week/main.py
@@ -0,0 +1,43 @@
+import random as rand
+import typing
+
+from Calculator import echo
+
+
+
+
+
+
+
+
+
+# from Calculator import Calculator
+# from Number import Number
+#
+# first_num: Number = Number(2)
+# second_num: Number = Number(3)
+#
+# calulator: Calculator = Calculator(
+#     first_num=first_num,
+#     second_num=second_num,
+# )
+#
+# calulator.add()
+# print(calulator.result)  # -> 5
+# calulator.mul()
+# print(calulator.result)  # -> 6
+# calulator.div()
+# print(calulator.result)  # -> 0.66666666
+# calulator.sub()
+# print(calulator.result)  # -> -1
+# print(first_num == calulator.result)  # -> False
+# number_list: typing.List = list()
+# for _ in range(10):
+#     number_list.append(
+#         Number(
+#             number=rand.randint(0, 1000),
+#         )
+#     )
+# print(number_list)  # -> [946, 170, 295, 592, 171, 754, 130, 800, 854, 716]
+# number_list.sort()
+# print(number_list)  # -> [130, 170, 171, 295, 592, 716, 754, 800, 854, 946]
