be662665b9a458c2b8b364ff88ecda77e728ed51
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-07-21 23:44:58 +0900

    Programmers L2 피보나치 수

--- Programmers/Level 2/피보나치 수.py
+++ Programmers/Level 2/피보나치 수.py
@@ -7,14 +7,6 @@ def solution(n):
             fibo = answer[i-1] + answer[i-2]
             answer.append(fibo % 1234567)
 
-    return answer[-1]
 
-# def sol2(n):
-#     a, b = 0, 1
-#     for i in range(n):
-#         a, b = b, a + b
-#
-#     return a
-#
-# print(solution(4))
-# print(sol2(4))
+
+    return answer[-1]
