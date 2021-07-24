4812ff4c4cdf4d316b8ce772711613464fd6cc2d
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-07-24 20:14:25 +0900

    Programmers L2 행렬곱하기

--- Programmers/Level 2/행렬 곱하기.py
+++ Programmers/Level 2/행렬 곱하기.py
@@ -1,5 +1,6 @@
 def sol(arr1, arr2):
     answer = [[0 for _ in range(len(arr2[0]))] for _ in range(len(arr1))]
+
     for i in range(len(arr1)):
         for j in range(len(arr2[0])):
             for k in range(len(arr1[0])):
