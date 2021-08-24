71264d1e56a2a0418b7b6af45e9469ae2dac221a
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-08-24 16:58:29 +0900

    Programmers L2 가장 큰 정사각형 찾기

---
+++ Programmers/Level 2/가장 큰 정사각형 찾기.py
@@ -0,0 +1,42 @@
+board = [[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]
+
+def sol(board):
+    n = len(board)
+    m = len(board[0])
+
+    dp = [[0]*m for _ in range(n)]
+    dp[0] = board[0]
+    for i in range(1, n):
+        dp[i] = board[i]
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if board[i][j] == 1:
+                dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+
+    answer = 0
+    for i in range(n):
+        temp = max(dp[i])
+        answer = max(answer, temp)
+
+    return answer**2
+
+
+def solution(board):
+    n = len(board)
+    m = len(board[0])
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if board[i][j] == 1:
+                board[i][j] = min(board[i - 1][j - 1], board[i - 1][j], board[i][j - 1]) + 1
+
+    answer = 0
+    for i in range(n):
+        temp = max(board[i])
+        answer = max(answer, temp)
+
+    return answer ** 2
+
+print(sol(board))
+
