5cf5004c13138fdb2aac10a420a6e18b4725d0cf
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-04 17:32:46 +0900

    Programmers L3 하노이의 탑

---
+++ Programmers/Level 2/하노이의 탑.py
@@ -0,0 +1,14 @@
+def solution(n):
+    def hanoi(n, F, T, S):
+        if n == 1:
+            answer.append([F, T])
+            return
+        hanoi(n - 1, F, S, T)
+        answer.append([F,T])
+        hanoi(n - 1, S, T, F)
+
+    answer = []
+    hanoi(n, 1, 3, 2)
+    return answer
+
+print(solution(3))
