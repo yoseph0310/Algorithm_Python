2ce42a9f8eba03d2fc18b91b57c6e12b699cec48
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-04 15:43:58 +0900

    Programmers L2 주식가격

---
+++ Programmers/Level 2/주식가격.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    queue = deque(prices)
+
+    while queue:
+        price = queue.popleft()
+        sec = 0
+        for q in queue:
+            sec += 1
+            if price > q:
+                break
+        answer.append(sec)
+
+    return answer
+
+prices = [1, 2, 3, 2, 3]
+print(solution(prices))
