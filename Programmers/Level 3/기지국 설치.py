e1824be0f0c7b32bcfd695056ab4b126f68bdbed
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-08 22:57:14 +0900

    :sparkles: Programmers L3 기지국 설치

---
+++ Programmers/Level 3/기지국 설치.py
@@ -0,0 +1,13 @@
+def solution(n, stations, w):
+    answer = 0
+    idx = 0
+    locate = 1
+    while locate <= n:
+        if idx < len(stations) and locate >= stations[idx]-w:
+            locate += stations[idx] + w + 1
+            idx += 1
+        else:
+            locate = 2*w + 1
+            answer += 1
+
+    return answer
