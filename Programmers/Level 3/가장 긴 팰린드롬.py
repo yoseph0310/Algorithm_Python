071f16359e1449111b5002a626c6a1e0b9c43aae
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-09 21:12:52 +0900

    :sparkles: Programmers L3 가장 긴팰린드롬

---
+++ Programmers/Level 3/가장 긴 팰린드롬.py
@@ -0,0 +1,14 @@
+def isPalin(x):
+    if x == x[::-1]:
+        return True
+
+def solution(s):
+    answer = 0
+
+    for i in range(len(s)):
+        for j in range(i+1, len(s)+1):
+            if isPalin(s[i:j]):
+                if answer < len(s[i:j]):
+                    answer = len(s[i:j])
+
+    return answer
