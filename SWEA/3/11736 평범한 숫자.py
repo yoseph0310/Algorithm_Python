8f6c1d1c8e2ff705fd149735ae109785e67c9044
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-10-16 17:58:44 +0900

    :sparkles: 11736 평범한 숫자

---
+++ SWEA/3/11736 평범한 숫자.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin =open('sample_input.txt', 'r')
+
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    p = list(map(int, input().split()))
+    cnt = 0
+
+    for q in range(1, N-1):
+        if p[q-1] < p[q] < p[q+1] or p[q+1] < p[q] < p[q-1]:
+            cnt += 1
+
+    print('#{} {}'.format(t, cnt))
