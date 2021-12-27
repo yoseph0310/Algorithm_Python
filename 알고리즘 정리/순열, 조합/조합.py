cb0ae0554f0fbad8bd084f18760e93c93716d8f7
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-12-27 14:32:53 +0900

    :sparkles: 조합

---
+++ 알고리즘 정리/순열, 조합/조합.py
@@ -0,0 +1,19 @@
+def combination(arr, r):
+    arr = sorted(arr)
+    used = [0 for _ in range(len(arr))]
+
+    def generate(chosen):
+        if len(chosen) == r:
+            print(chosen)
+            return
+
+        start = arr.index(chosen[-1]) + 1 if chosen else 0
+        for nxt in range(start, len(arr)):
+            if used[nxt] == 0 and (nxt == 0 or arr[nxt-1] != arr[nxt] or used[nxt-1]):
+                chosen.append(arr[nxt])
+                used[nxt] = 1
+                generate(chosen)
+                chosen.pop()
+                used[nxt] = 0
+
+    generate([])
