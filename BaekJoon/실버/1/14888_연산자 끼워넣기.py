b40da7a5e67b74002293cd5f464a84e67b00223e
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-12-27 23:39:26 +0900

    :sparkles: 14888_연산지 끼워넣기

---
+++ BaekJoon/실버/1/14888_연산자 끼워넣기.py
@@ -0,0 +1,39 @@
+import sys
+from itertools import permutations as perm
+
+input = sys.stdin.readline
+N = int(input())
+numbers = list(map(int, input().split()))
+op_num = list(map(int, input().split())) # 0 0 1 0
+op_list = ['+', '-', '*', '/']
+op = []
+
+for i in range(len(op_num)):
+    for j in range(op_num[i]):
+        op.append(op_list[i])
+
+maximum = -1e9
+minimum = 1e9
+
+def solve():
+    global maximum, minimum
+    for case in perm(op, N-1):
+        ans = numbers[0]
+        for r in range(1, N):
+            if case[r - 1] == '+':
+                ans += numbers[r]
+            elif case[r - 1] == '-':
+                ans -= numbers[r]
+            elif case[r - 1] == '*':
+                ans *= numbers[r]
+            elif case[r - 1] == '/':
+                ans = int(ans / numbers[r])
+
+        if ans > maximum:
+            maximum = ans
+        if ans < minimum:
+            minimum = ans
+
+solve()
+print(maximum)
+print(minimum)
