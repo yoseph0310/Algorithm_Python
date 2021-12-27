9911ccfda8f1741af13c472350e014c8beac4ad9
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-12-27 14:26:11 +0900

    :sparkles: 순열

--- 알고리즘 정리/순열, 조합/순열.py
+++ 알고리즘 정리/순열, 조합/순열.py
@@ -1,21 +1,21 @@
-def perm(number, d):
-    permutation[d] = number
-    if (d == n-1):
-        for i in permutation:
+def perm(number, digit):
+    array[digit] = number
+    if digit == n-1:
+        for i in array:
             print(i, end=' ')
         print()
         return
+
     for i in range(1, n+1):
-        if(visited[i]): continue
+        if visited[i]: continue
         visited[i] = True
-        perm(i, d+1)
+        perm(i, digit+1)
         visited[i] = False
 
 n = int(input())
-visited = [False for i in range(n+1)]
-permutation = [0] * n
-
-for i in range(1, n+1):
+visited = [False for i in range(n + 1)]
+array = [0] * n
+for i in range(1, n + 1):
     visited[i] = True
     perm(i, 0)
     visited[i] = False
