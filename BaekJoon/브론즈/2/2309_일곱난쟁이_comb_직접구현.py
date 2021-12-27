8880d0ce9797419de34c5e77a2a868c05f9877b4
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-12-27 18:31:37 +0900

    :recycle: 2309 일곱난쟁이 comb 직접구현

--- BaekJoon/브론즈/2/2309_일곱난쟁이_comb_직접구현.py
+++ BaekJoon/브론즈/2/2309_일곱난쟁이_comb_직접구현.py
@@ -5,8 +5,10 @@ def combination(arr, n):
         return [[]]
 
     for i in range(0, len(arr)):
+        # 전체 배열 중 첫 인덱스 값과 나머지 값들의 배열로 슬라이싱
         elem = arr[i]
         rest_arr = arr[i + 1:]
+        # 나머지 값들의 배열을 n-1만큼 재귀를 돌며 결과 배열들을 만들어낸다.
         for C in combination(rest_arr, n-1):
             result.append([elem]+C)
 
