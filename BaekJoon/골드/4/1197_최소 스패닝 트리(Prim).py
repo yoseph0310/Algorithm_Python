cb4af4d9b998b6c6f05cfddc592dfb1527d92a2e
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2022-01-10 14:51:37 +0900

    :sparkles: 1197 최소 스패팅 트리(Prim)

---
+++ BaekJoon/골드/4/1197_최소 스패닝 트리(Prim).py
@@ -0,0 +1,28 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+V, E = map(int, input().split())
+visited = [False] * (V + 1)
+Elist = [[] for _ in range(V + 1)]
+heap = [[0, 1]]
+for _ in range(E):
+    s, e, w = map(int, input().split())
+    Elist[s].append([w, e])
+    Elist[e].append([w, s])
+
+ans = 0
+cnt = 0
+while heap:
+    if cnt == V:
+        break
+    w, s = heapq.heappop(heap)
+
+    if not visited[s]:
+        visited[s] = True
+        ans += w
+        cnt += 1
+        for i in Elist[s]:
+            heapq.heappush(heap, i)
+
+print(ans)
